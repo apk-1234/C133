0251881ef0275771c3616d45a722a41ba83a8387
apk-1234 <81460947+apk-1234@users.noreply.github.com>
2022-03-26 13:04:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+}
+img="";
+objects=[];
+status="";
+percent=0;
+function preload()
+{
+    img=loadImage('dog_cat.jpg');
+}
+function modelLoaded()
+{
+    console.log("Model is Loaded!");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+function draw() 
+{
+    image(img, 0, 0, 640, 420);
+  
+       if(status != "")
+        {
+          for (i = 0; i < objects.length; i++) 
+          {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+      
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          }
+        }
+}
+function gotResult(results,error)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
